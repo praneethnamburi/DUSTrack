aed08c1ec7c3b64055caea8321dda5f166605e98
HaowenJohnWei <hw2892@columbia.edu>
2025-11-06 17:15:01 -0500

    server training and inference

--- pia02_workflow/pia02_inference/video_inference4.py
+++ pia02_workflow/pia02_inference/video_inference4.py
@@ -7,14 +7,14 @@ from tqdm import tqdm
 import deeplabcut
 
 # Add the parent directory to Python path so we can import dustrack
-parent_dir = Path(__file__).parent.parent
+parent_dir = Path(__file__).parent.parent.parent
 sys.path.insert(0, str(parent_dir))
 
 from dustrack import DUSTrack, DLCProject
 
 if __name__ == "__main__":
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "4"
     import torch
     # print the number of GPUs available
     print(f"Number of GPUs available: {torch.cuda.device_count()}")
@@ -29,37 +29,20 @@ if __name__ == "__main__":
         exit()
 
     dlcp = DLCProject(path = dlc_project_config_path)
-    # dlcp.extract_frames()
-    # dlcp.extract_frames()
-
-    # create training dataset
-
-    # net_type = 'resnet_50'
-    # epochs=300
-    # max_snapshots_to_keep = 20
-    # batch_size = 16
-
-    # deeplabcut.create_training_dataset(dlc_project_config_path, net_type=net_type)
-
-    # deeplabcut.train_network(dlc_project_config_path, epochs=epochs, max_snapshots_to_keep=max_snapshots_to_keep, shuffle=1, batch_size=batch_size)
-
-    # a = {'snapshotindex': 'all'}
-    # deeplabcut.auxiliaryfunctions.edit_config(configname=dlc_project_config_path, edits=a)
-    # deeplabcut.evaluate_network(config=dlc_project_config_path)
-
-
-    # # dlcp.evaluate()
 
     video_root_path = r"/home/hwjwei/scratch/hwjwei/pia02/DLC_MODELS/general/interosseous_pn24-x-2025-10-24/videos"
     video_list = [os.path.join(video_root_path, f) for f in os.listdir(video_root_path) if f.endswith('.mp4')]
-    videos = [video_list[0]]
-    dlcp.analyze_videos(iteration_num=0, create_video=False, batchsize=256)
+    videos = [video_list[50]]
+    dlcp.analyze_videos(iteration_num=0, create_video=False, batchsize=64, videos=videos)
 
 
 
 
-
-
+# taskset -c 1 python /home/hwjwei/projects/pia02/DUSTrack/pia02_workflow/pia02_inference/video_inference.py
+# taskset -c 1 python /home/hwjwei/projects/pia02/DUSTrack/pia02_workflow/pia02_inference/video_inference1.py
+# taskset -c 1 python /home/hwjwei/projects/pia02/DUSTrack/pia02_workflow/pia02_inference/video_inference2.py
+# taskset -c 1 python /home/hwjwei/projects/pia02/DUSTrack/pia02_workflow/pia02_inference/video_inference3.py
+# taskset -c 1 python /home/hwjwei/projects/pia02/DUSTrack/pia02_workflow/pia02_inference/video_inference4.py
 
 
 
