0239e4a4383172fa1a522e9c20e6757d4d1dcc6a
HaowenWeiJohn <59973823+HaowenWeiJohn@users.noreply.github.com>
2026-05-25 13:32:26 -0400

    update the pia02 local PC training script

--- pia02_workflow/dustrack_training.py
+++ pia02_workflow/dustrack_training.py
@@ -21,48 +21,26 @@ if __name__ == "__main__":
 
 
 
-    dlc_project_config_path = r"\\192.168.1.104\home\piano\DLC_MODELS\participant_models_general\s018\LFA\interosseous_pn24-x-2025-10-24\config.yaml"
-    source_model_path = r"\\192.168.1.104\home\piano\DLC_MODELS\participant_models_general\s018\LFA\interosseous_pn24-x-2025-10-24\dlc-models-pytorch\iteration-0\interosseous_pn24Oct24-trainset95shuffle1\train\snapshot-100.pt"
+    dlc_project_config_path = r"\\192.168.1.104\home\piano\DLC_MODELS\participant_models_general\s029\LFA\interosseous_pn24-x-2025-10-24\config.yaml"
+    source_model_path = r"\\192.168.1.104\home\piano\DLC_MODELS\participant_models_general\snapshot-best-270.pt"
+    import torch
+    # print the number of GPUs available
+    print(f"Number of GPUs available: {torch.cuda.device_count()}")
     # check if the project exists
     if not os.path.exists(dlc_project_config_path):
         print(f"Project config file {dlc_project_config_path} does not exist")
         exit()
 
     dlcp = DLCProject(path = dlc_project_config_path)
-    
-    dlcp.process(maxiters=100, refine = source_model_path)
 
-    # dlcp.extract_frames()
-    # dlcp.extract_frames()
+    dlcp.process(maxiters=100, analyse_batchsize=8, create_video=False, refine = source_model_path)
 
-    # # create training dataset
+    # from dustrack import _config
+    # _config.DLC3_USE_LAST_SNAPSHOT = False
+    # dlcp.analyze_videos(create_video=False, batchsize=args.analyze_batchsize)
 
-    # net_type = 'resnet_50'
-    # # epochs=300
-    # # max_snapshots_to_keep = 20
-    # # batch_size = 16
-
-    # deeplabcut.create_training_dataset(dlc_project_config_path, net_type=net_type)
-
-    # # deeplabcut.train_network(dlc_project_config_path, epochs=epochs, max_snapshots_to_keep=max_snapshots_to_keep, shuffle=1, batch_size=batch_size)
-
-    # # a = {'snapshotindex': 'all'}
-    # # deeplabcut.auxiliaryfunctions.edit_config(configname=dlc_project_config_path, edits=a)
-    # # deeplabcut.evaluate_network(config=dlc_project_config_path)
-
-
-    # # # dlcp.evaluate()
-
-    # video_root_path = r"/home/hwjwei/scratch/hwjwei/pia02/DLC_MODELS/general/interosseous_pn24-x-2025-10-24/videos"
-    # video_list = [os.path.join(video_root_path, f) for f in os.listdir(video_root_path) if f.endswith('.mp4')]
-    # videos = [video_list[0]]
-    # dlcp.analyze_videos(iteration_num=0, create_video=False, batchsize=256)
-
-
-
-
-
-
+    # # print complte message
+    print(f"Training completed for {dlc_project_config_path}")
 
 
 
